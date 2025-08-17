70f06abec51e6b6f8323b954f5bd735d860ca3e0
Federico Yacoubian <federicoyn@gmail.com>
2025-08-17 00:49:53 -0400

    test data

---
+++ src/app/fonts.tsx
@@ -0,0 +1,8 @@
+import { Zilla_Slab } from "next/font/google";
+
+export const zillaSlab = Zilla_Slab({
+  weight: ["400", "500", "700"], // choose the weights you need
+  subsets: ["latin"],
+  variable: "--font-zilla", // optional: sets a CSS variable
+});
+
